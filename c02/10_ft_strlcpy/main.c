fd3b98701aa1af0f3b7388c5cb881f85dbbcf8d8
Marsha <pgmarshateo@gmail.com>
2023-12-20 23:05:17 +0400

    uncomment strlcpy lines

--- c02/10_ft_strlcpy/main.c
+++ c02/10_ft_strlcpy/main.c
@@ -6,7 +6,7 @@
 /*   By: mateo <mateo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 16:11:48 by mateo             #+#    #+#             */
-/*   Updated: 2023/11/29 13:54:11 by mateo            ###   ########.fr       */
+/*   Updated: 2023/12/20 23:04:49 by mateo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,11 @@ int	main(void)
 	printf("Before: s = %s, d1 = %s, d2 = %s\n", s, d1, d2);
 	r = ft_strlcpy(d1, s, 0);
 	printf("After ft_strlcpy: return value: %u, s = %s, d1 = %s\n", r, s, d1);
-	// r = strlcpy(d2, s, 0);
-	// printf("After strlcpy:    return value: %u, s = %s, d2 = %s\n", r, s, d2);
+	r = strlcpy(d2, s, 0);
+	printf("After strlcpy:    return value: %u, s = %s, d2 = %s\n", r, s, d2);
 	r = ft_strlcpy(d1, s, 3);
 	printf("After ft_strlcpy: return value: %u, s = %s, d1 = %s\n", r, s, d1);
-	// r = strlcpy(d2, s, 3);
-	// printf("After strlcpy: 	  return value: %u, s = %s, d2 = %s\n", r, s, d2);
-	
+	r = strlcpy(d2, s, 3);
+	printf("After strlcpy: 	  return value: %u, s = %s, d2 = %s\n", r, s, d2);
 	return (0);
 }
